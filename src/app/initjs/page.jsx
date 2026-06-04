c8f1889d61201500034a1bd0a9e4769436a22291
Michael Mitrakos <mike.mitrakos@gmail.com>
2026-06-04 17:01:25 +0300

    update

--- src/app/initjs/page.jsx
+++ src/app/initjs/page.jsx
@@ -1,6 +1,6 @@
 import { Card } from '@/components/Card'
-import { SimpleLayout } from '@/components/SimpleLayout'
-import { StaggerContainer, StaggerItem } from '@/components/animations'
+import { Container } from '@/components/Container'
+import { FadeIn, StaggerContainer, StaggerItem } from '@/components/animations'
 import { formatDate } from '@/lib/formatDate'
 import { pageMetadata } from '@/lib/site'
 
@@ -41,23 +41,34 @@ export const metadata = pageMetadata({
 
 export default async function ArticlesIndex() {
   return (
-    <SimpleLayout
-      title="Writing on All Things Javascript"
-      intro="All of my long-form thoughts on programming in Javascript."
-    >
-      <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
-        <StaggerContainer
-          className="flex max-w-3xl flex-col space-y-16"
-          staggerDelay={0.1}
-          animate
-        >
-          {articles.map((article) => (
-            <StaggerItem key={article.url}>
-              <Article article={article} />
-            </StaggerItem>
-          ))}
-        </StaggerContainer>
+    <Container className="mt-16 sm:mt-32">
+      <header className="max-w-2xl">
+        <FadeIn direction="fade-up">
+          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
+            Writing on All Things Javascript
+          </h1>
+        </FadeIn>
+        <FadeIn direction="fade-up" delay={0.1}>
+          <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+            All of my long-form thoughts on programming in Javascript.
+          </p>
+        </FadeIn>
+      </header>
+      <div className="mt-16 sm:mt-20">
+        <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
+          <StaggerContainer
+            className="flex max-w-3xl flex-col space-y-16"
+            staggerDelay={0.1}
+            animate
+          >
+            {articles.map((article) => (
+              <StaggerItem key={article.url}>
+                <Article article={article} />
+              </StaggerItem>
+            ))}
+          </StaggerContainer>
+        </div>
       </div>
-    </SimpleLayout>
+    </Container>
   )
 }
